4dda779cc6fcd2956ac2febb0af0bffd303c0bc9
Theophilus Eriata <theoeriata@gmail.com>
2022-10-11 01:37:16 -0600

    Implement Manual Drive Opmode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/manual/ManualDrive.java
@@ -0,0 +1,206 @@
+package org.firstinspires.ftc.teamcode.opmode.manual;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.control.util.Extensions;
+import org.firstinspires.ftc.teamcode.opmode.Defines;
+import org.firstinspires.ftc.teamcode.opmode.OpModeBase;
+
+public class ManualDrive extends OpModeBase
+{
+  @Override
+  public void registerAccessors()
+  {
+
+  }
+
+  @Override
+  public void registerSubsystems()
+  {
+
+  }
+
+  @Override
+  public void update()
+  {
+    updateMovementState();
+
+    updateAccessors();
+
+    // Update telemetry
+    {
+      telemetry.addData(">", "Run Time (s) (%.2f)", getRuntime());
+      telemetry.addData(">", "Motors: Left Front Power (%.2f), Right Front Power (%.2f)", driveEngine.getLeftFrontMotor().getPower(), driveEngine.getRightFrontMotor().getPower());
+      telemetry.addData(">", "Motors: Left Back Power (%.2f), Right Back Power (%.2f)", driveEngine.getLeftRearMotor().getPower(), driveEngine.getRightRearMotor().getPower());
+    }
+  }
+
+  /*
+   * Update translation and rotation of the robot.
+   */
+  protected void updateMovementState()
+  {
+    // [-1: Up,   1: Down]
+    // [-1: Left, 1: Right]
+
+    switch (Defines.DRIVE_MODE)
+    {
+      case ROBOT_CENTRIC_HOLONOMIC:
+      {
+        double y  = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double x  = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator     = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double leftFrontPower  = (y + x + rx) / denominator;
+        double leftRearPower   = (y - x + rx) / denominator;
+        double rightFrontPower = (y - x - rx) / denominator;
+        double rightRearPower  = (y + x - rx) / denominator;
+
+        driveEngine.setMotorPowers(leftFrontPower * Defines.DRIVE_COEFFICIENT,
+                                   leftRearPower * Defines.DRIVE_COEFFICIENT,
+                                   rightRearPower * Defines.DRIVE_COEFFICIENT,
+                                   rightFrontPower * Defines.DRIVE_COEFFICIENT
+                                  );
+      }
+      break;
+
+      case ROBOT_CENTRIC_MECANUM:
+      {
+        double length     = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double rightX     = gamepad1.right_stick_x;
+
+        final double leftFrontPower  = length * Math.cos(robotAngle) + rightX;
+        final double rightFrontPower = length * Math.sin(robotAngle) - rightX;
+        final double leftRearPower   = length * Math.sin(robotAngle) + rightX;
+        final double rightRearPower  = length * Math.cos(robotAngle) - rightX;
+
+        driveEngine.setMotorPowers(leftFrontPower * Defines.DRIVE_COEFFICIENT,
+                                   leftRearPower * Defines.DRIVE_COEFFICIENT,
+                                   rightRearPower * Defines.DRIVE_COEFFICIENT,
+                                   rightFrontPower * Defines.DRIVE_COEFFICIENT
+                                  );
+      }
+      break;
+
+      case FIELD_CENTRIC_GAMEPAD:
+      {
+        // Get current position estimate
+        Pose2d currentPosition = driveEngine.getPoseEstimate();
+
+        // Get vector direction, derived from the (x, y) positions on the gamepad control axes
+        // And rotate the vector by the inverse of the current heading
+        Vector2d inputVector = new Vector2d(-gamepad1.left_stick_x, gamepad1.left_stick_y).rotated(-currentPosition.getHeading());
+
+        // Pass in the rotated input + right stick value for rotation
+        // Rotation is not part of the rotated input, thus, it must be passed in separately
+        driveEngine.setWeightedDrivePower(new Pose2d(
+            inputVector.getX() * Defines.DRIVE_COEFFICIENT,
+            inputVector.getY() * Defines.DRIVE_COEFFICIENT,
+            -gamepad1.right_stick_x * Defines.DRIVE_COEFFICIENT
+        ));
+
+        telemetry.addData("> Pos X", currentPosition.getX());
+        telemetry.addData("> Pos Y", currentPosition.getY());
+        telemetry.addData("> Heading", currentPosition.getHeading());
+      }
+      break;
+
+      case FIELD_CENTRIC_IMU:
+      {
+        double heading = driveEngine.getHeadingOffset(robotAngleOffset);
+
+        if (gamepad1.left_bumper && gamepad1.a && !previousState)
+        {
+          robotAngleOffset += heading;
+        }
+
+        previousState = gamepad1.left_bumper && gamepad1.a;
+
+        double y  = (Math.abs(gamepad1.left_stick_y) > 0.05) ? Extensions.cubeInput(-gamepad1.left_stick_y, 0.4) : 0.0; // Remember, this is reversed!
+        double x  = (Math.abs(gamepad1.left_stick_x) > 0.05) ? Extensions.cubeInput(gamepad1.left_stick_x * 1.1, 0.4) : 0.0; // Counteract imperfect strafing
+        double rx = (Math.abs(gamepad1.right_stick_x) > 0.05) ? Extensions.cubeInput(gamepad1.right_stick_x, 0.4) : 0.0;
+
+        // Get the field centric inputs
+        Pose2d pose = Extensions.toFieldRelative(new Pose2d(x, y, rx), heading);
+
+        x  = pose.getX();
+        y  = pose.getY();
+        rx = pose.getHeading();
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+        double frontLeftPower  = (y + x + rx) / denominator;
+        double backLeftPower   = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower  = (y + x - rx) / denominator;
+
+        driveEngine.setMotorPowers(
+            frontLeftPower * Defines.DRIVE_COEFFICIENT,
+            backLeftPower * Defines.DRIVE_COEFFICIENT,
+            backRightPower * Defines.DRIVE_COEFFICIENT,
+            frontRightPower * Defines.DRIVE_COEFFICIENT
+                                  );
+
+        telemetry.addLine("> Press the left bumper to re-zero the heading.");
+        telemetry.addData("> Current Heading with offset", AngleUnit.DEGREES.fromRadians(driveEngine.getHeadingOffset(robotAngleOffset)));
+        telemetry.addData("> Offset", AngleUnit.DEGREES.fromRadians(robotAngleOffset));
+      }
+      break;
+
+      default:
+        break;
+    }
+  }
+
+  /*
+   * Update Accessors.
+   */
+  public void updateAccessors()
+  {
+    // Update drive coefficient through the Gamepad.
+    {
+      // Speed limiter accessor update
+      if (!gamepad1.left_bumper)
+      {
+        if (gamepad1.dpad_up)
+        {
+          Defines.DRIVE_COEFFICIENT = 1.0f;
+        } else if (gamepad1.dpad_right)
+        {
+          Defines.DRIVE_COEFFICIENT = 0.75f;
+        } else if (gamepad1.dpad_down)
+        {
+          Defines.DRIVE_COEFFICIENT = 0.5f;
+        } else if (gamepad1.dpad_left)
+        {
+          Defines.DRIVE_COEFFICIENT = 0.25f;
+        }
+      } else
+      {
+        if (gamepad1.dpad_up)
+        {
+          Defines.DRIVE_COEFFICIENT = (0.5f + 0.25f + 0.75f + 1.0f) / 2.0f;
+        } else if (gamepad1.dpad_right)
+        {
+          Defines.DRIVE_COEFFICIENT = (0.5f + 0.25f + 0.75f) / 2.0f;
+        } else if (gamepad1.dpad_down)
+        {
+          Defines.DRIVE_COEFFICIENT = (0.5f + 0.25f) / 2.0f;
+        } else if (gamepad1.dpad_left)
+        {
+          Defines.DRIVE_COEFFICIENT = 0.25f / 2.0f;
+        }
+      }
+    }
+  }
+}
