c1e442994b6f023a4c7da89fec4b57d3e239edf4
kohinoor2005 <Kohinoorc0110@gmail.com>
2022-11-04 15:21:08 -0600

    Add Autonomous Test.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/tests/AutoTest.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.opmode.autonomous.tests;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.opmode.OpModeBase;
+
+@Config
+@Autonomous(name = "Autotest", group = "Autonomous")
+public class AutoTest extends OpModeBase
+{
+  public static AutoState state = AutoState.Stop;
+
+  @Override
+  public void update()
+  {
+    switch (state)
+    {
+      case Forward:
+      {
+        driveEngine.setMotorPowers(1, 1, 1, 1);
+      }
+      break;
+
+      case Stop:
+      {
+        driveEngine.setMotorPowers(0, 0, 0, 0);
+      }
+      break;
+
+      default:
+      {
+        driveEngine.setMotorPowers(0, 0, 0, 0);
+      }
+    }
+  }
+
+  public enum AutoState
+  {
+    Forward,
+    Stop
+  }
+}
