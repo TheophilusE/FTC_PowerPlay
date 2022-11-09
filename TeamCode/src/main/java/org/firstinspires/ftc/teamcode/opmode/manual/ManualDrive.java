bd5567245c33d11b8d0bbd2bd4643941f8b1fce6
Theophilus Eriata <theoeriata@gmail.com>
2022-11-09 11:09:19 -0700

    Remove unused import.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/manual/ManualDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/manual/ManualDrive.java
@@ -8,7 +8,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.control.util.Extensions;
 import org.firstinspires.ftc.teamcode.hardware.subsystems.LiftSubsystem;
-import org.firstinspires.ftc.teamcode.math.FastMath;
 import org.firstinspires.ftc.teamcode.opmode.Defines;
 import org.firstinspires.ftc.teamcode.opmode.OpModeBase;
 
