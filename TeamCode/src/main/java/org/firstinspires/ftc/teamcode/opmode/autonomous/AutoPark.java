859a5f0a6a8d2f8dc823c0391c99db4431dae007
kohinoor2005 <Kohinoorc0110@gmail.com>
2022-11-10 16:59:57 -0700

    Add basic park autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/AutoPark.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.opmode.autonomous;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.control.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.hardware.commands.FollowTrajectoryCommand;
+import org.firstinspires.ftc.teamcode.hardware.commands.FollowTrajectorySequenceCommand;
+import org.firstinspires.ftc.teamcode.opmode.Defines;
+import org.firstinspires.ftc.teamcode.opmode.OpModeBase;
+
+@Config
+@Autonomous(name = "AutoPark", group = "Autonomous")
+public class AutoPark extends OpModeBase
+{
+  TrajectorySequence goToParkZone = null;
+
+  @Override
+  public void initialize()
+  {
+    super.initialize();
+
+    if (!Defines.FSM_STATE_OVERRIDE)
+    {
+      Defines.autonomousFSM = Defines.AutonomousFSM.IDLE;
+    }
+    
+    goToParkZone = driveEngine.trajectorySequenceBuilder(driveEngine.getPoseEstimate())
+        .strafeRight(48)
+        .build();
+
+    schedule(new SequentialCommandGroup(
+        new FollowTrajectorySequenceCommand(driveEngine, goToParkZone)
+    ));
+  }
+
+  @Override
+  public void update()
+  {
+    // Update Finite State Machine
+    updateFSM();
+  }
+
+  public void updateFSM()
+  {
+    switch (Defines.autonomousFSM)
+    {
+      default:
+      {
+        driveEngine.setZeroPower();
+      }
+      break;
+    }
+  }
+}
